0fd9013f9bb2c22a537fabd70ec47caa9d595bf7
FcoHernan <FranciscoHRF@gmail.com>
2015-05-19 18:34:52 -0300

    cambio de permisos y config

--- wp-config.php
+++ wp-config.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
-
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'prodevices_db');
@@ -22,10 +7,10 @@ define('DB_NAME', 'prodevices_db');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'taisa');
+define('DB_PASSWORD', 'rsu2015');
 
 /** MySQL hostname */
-define('DB_HOST', 'local.ecommerce.cl');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to uselocalhost in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -33,15 +18,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
 define('AUTH_KEY',         '_x*[*]_Ly:~Xp5D^F4!Iz`mOZ+qT;Lu{?dojP_-TabI{,G-H;m9*YY)FG7BNbS0X');
 define('SECURE_AUTH_KEY',  'A.q@>ujHO|>h/@P?/N:27?xQ$;L;oGroqeiPV1@+z u%?0T&VPUWa&g-~oh ]Pso');
 define('LOGGED_IN_KEY',    '+&+)Qj+b-^BOjXUVZzvninIcu?RO-S4kmDX@dO-aP5]1Am3Q:xSH|3>3-U1Z=4E9');
